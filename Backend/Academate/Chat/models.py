c216e8c16527a4218d296c3112417c4f9e1c7f84
gebby123 <tefayegebrehiwot123@gmail.com>
2024-03-31 17:53:54 +0300

    chat in progress

--- Backend/Academate/Chat/models.py
+++ Backend/Academate/Chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from shortuuidfield import ShortUUIDField
+from Academate.User.models import User
+from Academate.Chat.models import ChatRoom,ChatMessage
 
 # Create your models here.
+class ChatRoom(models.Model):
+   roomId =ShortUUIDField(primary_key=True)
+   type = models.CharField(max_length=10,default ='Group Chat')
+   member = models.ManyToManyField(User,related_name='member')
+   name= models.CharField(max_length=20,null=True,blank=True) 
+   
+   def __str__(self):
+       return self.roomId + '->'+str(self.name)
+   class ChatMessage(models.Model):
+       chat = models.ForeignKey(ChatRoom, on_delete=models.SET_NULL,null=True)
+       user = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+       message = models.CharField(max_length=255)
+       timestamp = models.DateTimeField(auto_now_add=True)
+       def __str__(self):
+           return self.message
+       
