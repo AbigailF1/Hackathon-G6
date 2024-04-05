d04e501295f91ce1c458d4cf8c7a4b587c2348d8
Abigail F.Hassen <bigidovi@gmail.com>
2024-04-05 11:39:35 +0300

    merge push

--- Backend/Academate/User/models.py
+++ Backend/Academate/User/models.py
@@ -4,10 +4,7 @@ from django.contrib.auth import get_user_model
 #from django.contrib.auth.base_user import BaseUserManager
 from rest_framework_simplejwt.tokens import RefreshToken
 
-
-
-
-
+    
 AUTH_PROVIDERS = {'facebook': 'facebook', 'google': 'google',
                   'twitter': 'twitter', 'email': 'email'}
 
