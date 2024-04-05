accc2ff52bbc34d26f5e45754256e17d74416f93
gebby123 <tefayegebrehiwot123@gmail.com>
2024-04-05 12:36:42 +0300

    user and chat plus chatbot merged

--- Backend/Academate/Academate/urls.py
+++ Backend/Academate/Academate/urls.py
@@ -37,6 +37,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('User.urls')),
+     path('api/', include('Chat.urls')),
+      path('api/', include('chatbot.urls')),
     path('', schema_view.with_ui('swagger',
                                  cache_timeout=0), name='schema-swagger-ui'),
 
