abee72b645d2b8dbd1040dd09af8ac7709ba8219
yohannaaaa <yohannamerawi@gmail.com>
2024-04-06 18:57:31 +0300

    landing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,36 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 //react router dom
-import{ createBrowserRouter, RouterProvider, createRoutesFromElements, Outlet, Route, useLocation,}from 'react-router-dom'
-import '@mantine/core/styles.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import "@mantine/core/styles.css";
 
-import Home from './pages/Landing';
-import Favorites from './pages/Home';
-import Login from './pages/Login';
+import Home from "./pages/Landing";
+import Favorites from "./pages/Home";
+import Login from "./pages/Login";
 
-import Notifications from './pages/Notifications';
+import Notifications from "./pages/Notifications";
 //import  Post from './pages/Post';
-import  Post from './pages/Posts';
-import Feed from './pages/Feed';
-import Chat from './pages/Chat';
-import Collaborate from './pages/Collaborate';
-import Idea from './pages/Ideas';
-import Header from './components/Header (2)';
-import SentConnection from './components/Connections/SentConnection';
-import Signup from './pages/Signup';
-import ForgotPassword from './pages/ForgotPassword';
+import Post from "./pages/Posts";
+import Feed from "./pages/Feed";
+import Chat from "./pages/Chat";
+import Collaborate from "./pages/Collaborate";
+import Idea from "./pages/Ideas";
+import Header from "./components/Header (2)";
+import SentConnection from "./components/Connections/SentConnection";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
 import ProfilePage from "./pages/ProfilePage";
-import Landing from './pages/Landing';
+import Landing from "./pages/Landing";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
-         <Route index element={<Home />} /> 
+        <Route index element={<Home />} />
 
         <Route path="/Login" element={<Login />} />
-        <Route path="/Signup" element={<Signup/>}/>
-        <Route path='/Forgot' element={<ForgotPassword/>}/>
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Forgot" element={<ForgotPassword />} />
         <Route path="/Collaborate" element={<Collaborate />} />
         <Route path="/SentConnection" element={<SentConnection />} />
         <Route path="/Idea" element={<Idea />} />
@@ -38,43 +45,43 @@ function App() {
         <Route path="/Notifications" element={<Notifications />} />
         <Route path="/Chat" element={<Chat />} />
 
-        <Route path="/Login" element = {<Login/>} />
-        <Route path="/Collaborate" element = {<Collaborate/>} />
-        <Route path="/Feed" element = {<Feed/>} />
-        <Route path="/Chat" element = {<Chat/>} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Collaborate" element={<Collaborate />} />
+        <Route path="/Feed" element={<Feed />} />
+        <Route path="/Chat" element={<Chat />} />
         <Route path="/Profile" element={<ProfilePage />} />
-        <Route path="/Landing" element = {<Landing/>}/>
-
-
+        <Route path="/Landing" element={<Landing />} />
       </Route>
     )
   );
   return (
     <>
-        <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 export default App;
 
-  const Root=()=>{
-    const location =useLocation();
-    const headerVisiblePaths = [  
-      "/Collaborate",
-      "/SentConnection", 
-      "/Idea", 
-      "/Post",
-       "/Notifications", 
-       "/Chat", 
-       "/Collaborate", 
-       "/Feed", 
-       "/Chat",
-      ];
-    const isHeaderVisible = headerVisiblePaths.includes(location.pathname);
-    return(
-      <>
+const Root = () => {
+  const location = useLocation();
+  const headerVisiblePaths = [
+    "/Collaborate",
+    "/SentConnection",
+    "/Idea",
+    "/Post",
+    "/Notifications",
+    "/Chat",
+    "/Collaborate",
+    "/Feed",
+    "/Chat",
+  ];
+  const isHeaderVisible = headerVisiblePaths.includes(location.pathname);
+  return (
+    <>
       {isHeaderVisible && <Header />}
-      <div><Outlet/></div>
-      </>
-    )
-  }
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
