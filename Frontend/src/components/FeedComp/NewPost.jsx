16feae26440d3097bba444b717fc3a84f881b9a8
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2024-04-11 11:10:09 +0300

    Added tags

--- Frontend/src/components/FeedComp/NewPost.jsx
+++ Frontend/src/components/FeedComp/NewPost.jsx
@@ -1,13 +1,48 @@
 import React, { useState } from 'react';
+import {  Tag, Input } from 'antd';
 import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 
 export default function NewPost() {
   const [post, setPost] = useState('');
+  const tagsData=[
+    'Web Dev',
+    'SQL',
+    'Python',
+    'Space',
+    'JavaScript',
+    'React',
+    'App dev',
+    'Flutter',
+    'Java',
+  ];
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [newTagInputVisible, setNewTagInputVisible] = useState(false);
+  const [newTagInputValue, setNewTagInputValue] = useState('');
+
+  const handleChange = (tag, checked) => {
+    const nextSelectedTags = checked
+      ? [...selectedTags, tag]
+      : selectedTags.filter((t) => t !== tag);
+    console.log(nextSelectedTags);
+    setSelectedTags(nextSelectedTags);
+  };
 
   const handleInputChange = (event) => {
     setPost(event.target.value);
+  };
+
+  const handleNewTagInputChange = (event) => {
+    setNewTagInputValue(event.target.value);
+  };
+
+  const handleNewTagInputConfirm = () => {
+    if (newTagInputValue && !selectedTags.includes(newTagInputValue)) {
+      setSelectedTags([...selectedTags, newTagInputValue]);
+    }
+    setNewTagInputVisible(false);
+    setNewTagInputValue('');
   };
 
   const handleSubmit = (event) => {
@@ -28,28 +63,78 @@ export default function NewPost() {
           placeholder="Write freely..."
           value={post}
           onChange={handleInputChange}
-          
+
         />
+      </div>
+    
+      <div className="mb-5 ">
+        <p className="uppercase font-semibold font-serif text-xs ">Tags:</p>
+        <div className="grid grid-cols-4 md:grid-cols-8 gap-2 -ml-2 mt-4">
+          {tagsData.map((tag) => (
+            <Tag.CheckableTag
+              key={tag}
+              checked={selectedTags.includes(tag)}
+              onChange={(checked) => handleChange(tag, checked)}
+            >
+              {tag}
+            </Tag.CheckableTag>
+          ))}
+          {newTagInputVisible ? (
+            <Input
+              autoFocus
+              type="text"
+              size="small"
+              style={{ width: '100%' }}
+              value={newTagInputValue}
+              onChange={handleNewTagInputChange}
+              onBlur={handleNewTagInputConfirm}
+              onPressEnter={handleNewTagInputConfirm}
+            />
+          ) : (
+            <Tag
+              onClick={() => setNewTagInputVisible(true)}
+              style={{
+                background: '#f0f2f5',
+                borderStyle: 'dashed',
+              }}
+            >
+              + New Tag
+            </Tag>
+          )}
+        </div>
+      </div>
+      <div className="mt-4 mb-10">
+        <p className="uppercase font-semibold font-serif text-xs">Selected Tags:</p>
+        <div>
+          {selectedTags.map((tag) => (
+            <Tag
+              key={tag}
+              closable
+              onClose={() => handleChange(tag, false)}
+            >
+              {tag}
+            </Tag>
+          ))}
+        </div>
       </div>
       <div className="flex flex-1 gap-6 justify-end items-center pb-2 mb-2">
         <label htmlFor="fileInput">
-          <AttachFileOutlinedIcon sx={{ color: "gray", cursor: "pointer" }} />
+          <AttachFileOutlinedIcon sx={{ color: 'gray', cursor: 'pointer' }} />
         </label>
-          <input
-            type="file"
-            id="fileInput"
-            style={{ display: "none" }}
-          />    
-      <label htmlFor="imageInput">
-        <ImageOutlinedIcon sx={{ color: "gray" ,cursor: "pointer" }} />
-      </label>
-            <input
-              type="file"
-              accept="image/*"
-              id="imageInput"
-              style={{ display: "none" }}
-            /> 
-          <SendOutlinedIcon sx={{color: "rgb(5, 190, 250)", cursor: "pointer"}} onClick={handleSubmit}/>
+        <input type="file" id="fileInput" style={{ display: 'none' }} />
+        <label htmlFor="imageInput">
+          <ImageOutlinedIcon sx={{ color: 'gray', cursor: 'pointer' }} />
+        </label>
+        <input
+          type="file"
+          accept="image/*"
+          id="imageInput"
+          style={{ display: 'none' }}
+        />
+        <SendOutlinedIcon
+          sx={{ color: 'rgb(5, 190, 250)', cursor: 'pointer' }}
+          onClick={handleSubmit}
+        />
       </div>
     </div>
   );
