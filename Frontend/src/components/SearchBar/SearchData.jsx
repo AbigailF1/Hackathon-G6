073ade033d1efad7fd2b018c4a924862d4ad3f19
solabest <soltad65@gmail.com>
2024-04-13 10:24:55 -0700

    style search page

--- Frontend/src/components/SearchBar/SearchData.jsx
+++ Frontend/src/components/SearchBar/SearchData.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./SearchData.css";
 
 const UserSearchDisplayComponent = ({ users }) => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -16,7 +17,10 @@ const UserSearchDisplayComponent = ({ users }) => {
   };
 
   return (
-    <div className="user-list p-8">
+    <div
+      className="user-list p-8"
+      style={{ width: "100%", paddingLeft: "200px" }}
+    >
       <h1 className="text-2xl font-bold mb-4">User List</h1>
       <div className="search mb-4">
         <input
@@ -25,6 +29,7 @@ const UserSearchDisplayComponent = ({ users }) => {
           value={searchTerm}
           onChange={handleChange}
           className="border border-gray-300 rounded-md py-2 px-4 w-64 mr-2"
+          style={{ width: "80%" }}
         />
         <button
           onClick={handleSearch}
@@ -33,57 +38,85 @@ const UserSearchDisplayComponent = ({ users }) => {
           Search
         </button>
       </div>
-      <div className="user-cards">
+      <div className="user-cards" style={{ width: "88%" }}>
         {filteredUsers.length > 0 ? (
           filteredUsers.map((user) => (
             <div
               className="user-card border border-gray-300 rounded-md p-4 mb-4"
               key={user.id}
             >
-              <h2 className="text-xl font-semibold mb-2">{user.username}</h2>
-              <p>
-                <strong>Email:</strong> {user.email}
-              </p>
-              <p>
-                <strong>Is Verified:</strong> {user.is_verified ? "Yes" : "No"}
-              </p>
-              <p>
-                <strong>Is Active:</strong> {user.is_active ? "Yes" : "No"}
-              </p>
-              <p>
-                <strong>Is Staff:</strong> {user.is_staff ? "Yes" : "No"}
-              </p>
-              <p>
-                <strong>Role:</strong> {user.role}
-              </p>
-              <p>
-                <strong>First Name:</strong> {user.first_name}
-              </p>
-              <p>
-                <strong>Last Name:</strong> {user.last_name}
-              </p>
-              <p>
-                <strong>Phone Number:</strong> {user.phone_number}
-              </p>
-              <p>
-                <strong>Is Student:</strong> {user.is_student ? "Yes" : "No"}
-              </p>
-              <p>
-                <strong>Is Recruiter:</strong>{" "}
-                {user.is_recruiter ? "Yes" : "No"}
-              </p>
-              <p>
-                <strong>Is Banned:</strong> {user.is_banned ? "Yes" : "No"}
-              </p>
-              <p>
-                <strong>Created At:</strong> {user.created_at}
-              </p>
-              <p>
-                <strong>Updated At:</strong> {user.updated_at}
-              </p>
-              <p>
-                <strong>Auth Provider:</strong> {user.auth_provider}
-              </p>
+              <h2
+                className="text-xl font-semibold mb-2"
+                style={{
+                  textTransform: "uppercase",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  color: "black",
+                }}
+              >
+                {user.username}
+              </h2>
+              <div
+                className="userInformation"
+                style={{
+                  display: "flex",
+                  gap: "15px",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div className="about">
+                  <p>
+                    <strong>First Name:</strong> {user.first_name}
+                  </p>
+                  <p>
+                    <strong>Last Name:</strong> {user.last_name}
+                  </p>
+                  <p>
+                    <strong>Email:</strong> {user.email}
+                  </p>
+                  <p>
+                    <strong>Phone Number:</strong> {user.phone_number}
+                  </p>
+                </div>
+                <div className="additional">
+                  <p>
+                    <strong>Role:</strong> {user.role}
+                  </p>
+
+                  <p>
+                    <strong>Is Student:</strong>{" "}
+                    {user.is_student ? "Yes" : "No"}
+                  </p>
+                  <p>
+                    <strong>Is Recruiter:</strong>
+                    {user.is_recruiter ? "Yes" : "No"}
+                  </p>
+                  <p>
+                    <strong>Created At:</strong> {user.created_at}
+                  </p>
+                  <p>
+                    <strong>Updated At:</strong> {user.updated_at}
+                  </p>
+                  <p>
+                    <strong>Auth Provider:</strong> {user.auth_provider}
+                  </p>
+                </div>
+                <div className="status">
+                  <p>
+                    <strong>Is Verified:</strong>{" "}
+                    {user.is_verified ? "Yes" : "No"}
+                  </p>
+                  <p>
+                    <strong>Is Active:</strong> {user.is_active ? "Yes" : "No"}
+                  </p>
+                  <p>
+                    <strong>Is Staff:</strong> {user.is_staff ? "Yes" : "No"}
+                  </p>
+                  <p>
+                    <strong>Is Banned:</strong> {user.is_banned ? "Yes" : "No"}
+                  </p>
+                </div>
+              </div>
             </div>
           ))
         ) : (
